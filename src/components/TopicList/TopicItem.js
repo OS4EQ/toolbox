a2e8fc54e73c9b600a32c68ff4bc2b3fccc95028
paulazoo <paulazhu@college.harvard.edu>
2020-10-04 00:41:33 -0400

    edit and delete buttons on topic items

--- src/components/TopicList/TopicItem.js
+++ src/components/TopicList/TopicItem.js
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import Card from 'react-bootstrap/Card';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 // Redux
 
 // Style
+import './TopicList.css';
 
 // Custom Components
 
 function TopicItem({ topic, index }) {
+  const handleEdit = () => {
+    console.log('edit');
+  };
+
+  const handleDelete = () => {
+    console.log('delete');
+  };
+
   return (
     <Draggable draggableId={`id-${topic.id}`} index={index}>
       {(provided) => (
@@ -17,7 +27,19 @@ function TopicItem({ topic, index }) {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          {topic.title}
+          <Row>
+            <Col>
+              <Card.Title>{topic.title}</Card.Title>
+            </Col>
+            <Col className='topic-item-buttons-container'>
+              <Button variant='light' onClick={handleEdit}>
+                <FaEdit />
+              </Button>
+              <Button variant='light' onClick={handleDelete}>
+                <FaTrash />
+              </Button>
+            </Col>
+          </Row>
         </Card>
       )}
     </Draggable>
