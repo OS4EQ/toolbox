ded801a4d62848526186705a4295f86560b9e087
paulazoo <paulazhu@college.harvard.edu>
2020-10-11 11:01:34 -0400

    import typo

--- src/store/apollo.js
+++ src/store/apollo.js
@@ -1,5 +1,7 @@
-{ ApolloClient } from 'react-apollo';
+import { ApolloClient } from 'react-apollo';
 
-const apolloClient = new ApolloClient();
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:8000/graphql/', // your GraphQL Server
+});
 
 export default apolloClient;
